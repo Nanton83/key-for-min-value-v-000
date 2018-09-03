a6556a168740ebb9e6a66b6fc6045d54788247b0
Nick Romans <nicholas.romans@gmail.com>
2018-09-03 17:47:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_possible_key = nil 
-  lowest_possible_value = 0 
+  lowest_possible_value = nil 
 name_hash.each do |key, value|
     if lowest_possible_value == 0 || value < lowest_possible_value
     lowest_possible_value = value 
